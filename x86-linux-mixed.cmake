5701926ece367699a8caee830ceb7c062805b7f5
ReplayCoding <replaycoding@gmail.com>
2022-08-28 09:02:21 -0400

    fix sdl2 debug build
    why!?

--- x86-linux-mixed.cmake
+++ x86-linux-mixed.cmake
@@ -1,11 +1,11 @@
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_TARGET_ARCHITECTURE x86)
 set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
 
+# SDL2 should be a shared lib. We also have to set the buildtype for SDL,
+# because it will change the lib name in debug mode
 if (${PORT} MATCHES "sdl2")
-  # SDL2 should be a shared lib
+  set(VCPKG_BUILD_TYPE release)
   set(VCPKG_LIBRARY_LINKAGE dynamic)
-else()
-  set(VCPKG_LIBRARY_LINKAGE static)
 endif()
-
-set(VCPKG_CMAKE_SYSTEM_NAME Linux)
