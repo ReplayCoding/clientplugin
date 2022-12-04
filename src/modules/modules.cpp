1a6a1fd27bee665cfa2db4c5c692326506160852
ReplayCoding <replaycoding@gmail.com>
2022-12-04 13:42:36 -0800

    emplace_back??

--- src/modules/modules.cpp
+++ src/modules/modules.cpp
@@ -8,9 +8,7 @@ ModuleDesc* g_ModuleList = nullptr;
 ModuleManager::ModuleManager() {
   for (auto module_desc = g_ModuleList; module_desc != nullptr;
        module_desc = module_desc->next) {
-    auto module = module_desc->factory();
-
-    modules.emplace_back(std::move(module));
+    modules.push_back(module_desc->factory());
   }
 
   gfx_overlay = new GfxOverlayMod(&modules);
