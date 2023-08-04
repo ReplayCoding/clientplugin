86c0e818b5e2bdeb1abc675d5e4bf701bd493c3b
ReplayCoding <replaycoding@gmail.com>
2023-08-04 10:46:28 -0400

    whatever the shit this was

--- src/modules/partychat.cpp
+++ src/modules/partychat.cpp
@@ -11,8 +11,8 @@ class PartyChatMod : public IModule {
   PartyChatMod();
 
  private:
-  std::unique_ptr<AttachmentHookBoth> party_name_hook;
-  // std::unique_ptr<ReplacementHook> party_name_hook;
+  // std::unique_ptr<AttachmentHookBoth> party_name_hook;
+  std::unique_ptr<ReplacementHook> party_name_hook;
 };
 
 wchar_t* GetPlayerNameForSteamID_replace(wchar_t* name,
@@ -26,17 +26,17 @@ wchar_t* GetPlayerNameForSteamID_replace(wchar_t* name,
 thread_local TracyCZoneCtx party_ctx;
 
 PartyChatMod::PartyChatMod() {
-  party_name_hook = std::make_unique<AttachmentHookBoth>(
-      offsets::GetPlayerNameForSteamID,
-      [](InvocationContext context) {
-        TracyCZoneN(ctx, "GetPlayerNameForSteamID", true);
-        party_ctx = ctx;
-        // auto name = context.get_arg<wchar_t*>(0);
-        // printf("%ls ----\n", name);
-      },
-      [](InvocationContext context) { TracyCZoneEnd(party_ctx); });
-  // party_name_hook = std::make_unique<ReplacementHook>(
-  //     offsets::GetPlayerNameForSteamID, GetPlayerNameForSteamID_replace);
+  // party_name_hook = std::make_unique<AttachmentHookBoth>(
+  //     offsets::GetPlayerNameForSteamID,
+  //     [](InvocationContext context) {
+  //       TracyCZoneN(ctx, "GetPlayerNameForSteamID", true);
+  //       party_ctx = ctx;
+  //       // auto name = context.get_arg<wchar_t*>(0);
+  //       // printf("%ls ----\n", name);
+  //     },
+  //     [](InvocationContext context) { TracyCZoneEnd(party_ctx); });
+  party_name_hook = std::make_unique<ReplacementHook>(
+      offsets::GetPlayerNameForSteamID, GetPlayerNameForSteamID_replace);
 }
 
 REGISTER_MODULE(PartyChatMod)
